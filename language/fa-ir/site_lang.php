d466b34bb11fb4f995f4b08f4a4c2dee7fbb0f36
Mahdi Hosseini <ikttc@yahoo.com>
2016-10-11 00:00:38 +0330

    Create site_lang.php

---
+++ language/fa-ir/site_lang.php
@@ -0,0 +1,58 @@
+<?php if ( ! defined('BASEPATH')) exit('دسترسی مستقیم به اسکریپت مجاز نیست');
+/*NAVIGATION and META*/
+$lang['meta_desc'] = 'ابزاری جهت جلوگیری از اتلاف وقت توسعه دهندگان. این ابزار مرورگر‌های قدیمی را تشخیص داده و به کاربران توصیه می‌کند که آن را به‌روز کنند.';
+$lang['btn_closenav'] = 'بستن نوار پیمایش';
+$lang['mainNav_home'] = 'صفحه‌اصلی';
+$lang['mainNav_project'] = 'پروژه';
+$lang['mainNav_how'] = 'نحوه استفاده';
+$lang['mainNav_faq'] = 'سوالات متداول';
+$lang['mainNav_git'] = 'صفحه گیت‌هاب';
+/*SHARE*/
+$lang['share_share'] = 'اشتراک گذاری';
+$lang['share_twitter'] ='ابزاری جهت جلوگیری از اتلاف وقت %23توسعه‌دهندگان%23. این ابزار %23مرورگر های %23قدیمی را شناسایی کرده و به کاربران توصیه می‌کند که آن را %23به‌روز کنند.';
+$lang['share_twitter_t'] = 'اشتراک گذاری در توییتر';
+$lang['share_fb_t'] = 'اشتراک گذاری در فیسبوک';
+/*GENERIC*/
+$lang['txt_close'] = 'بستن';
+$lang['txt_keepcalm'] = 'آرامش خود را حفظ کنید <br><span class="xs">این ابزار برای </span>کامپیوتر<br><span class="xs">است نه </span> موبایل';
+/*HOMEPAGE*/
+$lang['home_subtitle'] = 'برای نمایش بهتر وبسایت‌ها، مرورگر خود را به‌روز نمایید.';
+$lang['home_subtitle_nojs'] = 'جاوا اسکریپ مرورگر شما غیر فعال است، برای استفاده‌ی بهتر آن را فعال کنید.';
+$lang['home_subtitle_mobile'] = 'این ابزار برای کامپیوتر‌ها است, <br>ولی شما هم می‌توانید از آن دیدن کنید.';
+$lang['home_percentage'] = 'درصد استفاده مردم <br>از این مرورگر';
+$lang['home_download'] = 'دانلود';
+$lang['home_version'] = 'نسخه';
+$lang['home_available'] = 'قابل نصب در';
+/*PROJECT*/
+$lang['proj_title1'] = 'از جان <br> گذشته!';
+$lang['proj_desc1'] = 'بعد از گذراندن زمان زیاد برای بهینه سازی سایت برای مرورگر‌های قدیمی, تصمیم گرفتیم راه حلی پیدا کنید که در زمان شما توسعه دهندگان صرفه جویی بشه و بتونید زودتر به خونه هاتون برید. <br><br> که شامل پیتزا نمیشه!\'';
+$lang['proj_alt1'] = 'از جان گذشته';
+$lang['proj_desc1_2'] = 'توسعه دهنده دیوانه میشه!';
+$lang['proj_title2'] = 'چطور <br>کار می‌کنه?';
+$lang['proj_desc2'] = 'وقتی که اسکریپت اجرا بشه، چک می‌کنه که آیا ویژگی های css به خوبی پشتیبانی می‌شن.  اگر پشتیبانی از ویژگی های css صورت نگیره یک اخطار با ظاهر زیبا برای کاربر به نمایش درمیاد.';
+$lang['proj_desc2_2'] = 'یک سایت روی مرورگر‌های قدیمی اجرا می‌شود.';
+$lang['proj_title3'] = 'سه قدم <br> برای استفاده!';
+$lang['proj_desc3'] = 'فقط 3 دقیقه وقت می‌گیره. اضافه کردن فایل های css ، js و کمی کد HTML به قالب. <br><br>اطلاعات بیشتر <a class="github" href="https://github.com/burocratik/outdated-browser" rel="external" data-track="Github">گیت‌هاب</a>  ;)';
+$lang['proj_desc3_2'] = 'توسعه داده شده در <br> Bürolabs';
+/*HOW*/
+$lang['how_title1'] = 'دانلود <br> پکیج';
+$lang['how_desc1'] = "این پکیج شامل تمام فایل های JS،CSS و فایل های زبان است.";
+$lang['how_desc1_2'] = 'دانلود از گیت‌هاب';
+$lang['how_title2'] = 'اونارو به <br> پروژتون اضافه کنید';
+$lang['how_desc2'] = 'فایل css را در تگ &lt;head&gt; و فایل js را قبل از پایان تگ &lt;body&gt; قرار بدید.';
+$lang['how_desc2_2'] = 'کپی کد';
+$lang['how_alt2'] = 'مرورگر ';
+$lang['how_title3'] = 'کد <br> HTML <br> را اضافه کنید';
+$lang['how_desc3'] = 'این کد را قبل از پایان تگ &lt;/body&gt; tag. قرار بدید';
+$lang['how_desc3_2'] = '
+    <h6>مرورگر شما قدیمی است!</h6>
+<p>برای نمایش صحیح سایت مرورگر خود را به روز کنید. <a id="btnUpdateBrowser" href="http://outdatedbrowser.com/">مرورگرم را به روز کن!</a></p>
+<p class="last"> <a href="#" id="btnCloseUpdateBrowser" title="Close"> &times; </a> </p>
+';
+$lang['how_title4'] = 'Call the <br> Plugin and';
+$lang['how_alt4'] = 'تمام';
+$lang['how_desc4'] = 'از الان به بعد هرکس با مرورگر قدیمی وارد سایت شما بشه با یک اخطار زیبا مواجه می‌شه که مرورگر خودش رو به روز کنه.<br><br> اگر از این پروژه خوشتون اومده، اونو با بقیه به اشتراک بگذارید و چنگال بزنید!';
+$lang['how_desc4_2'] = '//تابع پلاگین (در DOM ready قرار دهید)';
+/*404*/
+$lang['url404'] = "http://outdatedbrowser.com/fa-ir";
+$lang['txt_keepcalm_error'] = 'آرامش خود را حفظ کنید <br><span class="xs">خطای 404</span>صفحه‌ای که <br> به دنبال آن می‌گردید<br><span class="xs">پیدا  نشد</span>';
